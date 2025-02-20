a28a4fb3bb23e7e38688c24124162a59274c62bc
Singh Pooja <singhpooja2944@gmail.com>
2025-02-20 12:18:15 +0530

    Industry Insights Weekly update function done..

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,15 @@
-// api folder: Here we build private nad public APIS
+// api folder: Here we build private and public APIS
 // we will build public API for inngest to connect to our application
 //  we will be using inngest for fetching Industry insights every week
+
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
-    helloWorld
+    /* your functions will be passed here later!. These functions will run in background */
+    generateIndustryInsights,
   ],
 });
